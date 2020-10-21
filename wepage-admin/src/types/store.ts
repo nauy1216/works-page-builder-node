12282bf17034f61c7cacfcbec2bcb31fee863fa3
liuchengyuan <liucy@rp-pet.cn>
2020-10-21 18:53:37 +0800

    update

--- wepage-admin/src/types/store.ts
+++ wepage-admin/src/types/store.ts
@@ -1,13 +1,20 @@
 import { mapMutations, mapState } from "vuex";
 import { State, Mutations } from "../store";
+import { PageStoreType } from "../store/page";
+import { EditorStoreType } from "../store/editor";
 
-type PickState<U, T extends keyof U> = () => Pick<U, T>[T];
+// 定义的模块在这里添加
+type ModulesState = StateNamespace<"page", PageStoreType["state"]> 
+  & StateNamespace<"editor", EditorStoreType["state"]>
+
+type ModulesMutation = MutationNamespace<"page", PageStoreType["mutations"]> 
+  & MutationNamespace<"editor", EditorStoreType["state"]> 
 
 // ==================== mapStateTyped ===================
 /**
   使用方式, 
-  // TODO 暂时没有支持命名空间， 后续有时间再支持
   computed: {
+    ...mapStateTyped("page", ["editorConfig"]),
     ...mapStateTyped({
       myPageConfig: "pageConfig"
     }),
@@ -38,27 +45,46 @@ interface MapperForState {
   ): { [K in keyof Map]: InlineComputed<Map[K]> };
 }
 
-type ComputedFunc<T extends keyof State> = PickState<State, T>;
+type ComputedFunc<State, Prop extends keyof State> = () => Pick<
+  State,
+  Prop
+>[Prop];
 interface StateMapper {
-  <Key extends keyof State>(map: Key[]): { [K in Key]: ComputedFunc<K> };
+  <Key extends keyof State>(map: Key[]): { [K in Key]: ComputedFunc<State, K> };
   <Map extends Record<string, keyof State>>(map: Map): {
-    [K in keyof Map]: ComputedFunc<Map[K]>;
+    [K in keyof Map]: ComputedFunc<State, Map[K]>;
   };
 }
 
-let _mapStateTyped: MapperForState & StateMapper;
+type InferModuleSateType<T> = T extends (
+  ...args: any[]
+) => infer P
+  ? P
+  : T extends undefined
+  ? any
+  : T;
 
-if (process.env.NODE_ENV === "production") {
-  _mapStateTyped = mapState;
-} else {
-  _mapStateTyped = function(...args: any[]) {
-    // @ts-ignore
-    const res = mapState(...args) as any;
-    return res;
-  };
+type StateNamespace<ModuleName, ModuleType> = <Key extends keyof ModuleType>(namespace: ModuleName, map: Key[]) => {
+  [K in Key]: () => InferModuleSateType<Pick<ModuleType, K>[K]>;
 }
 
-export const mapStateTyped = _mapStateTyped;
+interface StateMapperWithNamespace extends ModulesState {
+
+}
+
+const _mapStateTyped = mapState;
+export const mapStateTyped: MapperForState &
+  StateMapper &
+  StateMapperWithNamespace =
+  process.env.NODE_ENV === "production"
+    ? _mapStateTyped
+    : function(...args: any[]) {
+        // @ts-ignore
+        const res = _mapStateTyped(...args) as any;
+        return res;
+      };
+
+
 
 // ==================== mapMutationsTyped ===================
 /**
@@ -78,16 +104,23 @@ interface MutationMapper {
   };
 }
 
-let _mapMutationsTyped: MutationMapper;
-
-if (process.env.NODE_ENV === "production") {
-  _mapStateTyped = mapMutations;
-} else {
-  _mapMutationsTyped = function(...args: any[]) {
-    // @ts-ignore
-    const res = mapMutations(...args) as any;
-    return res;
-  };
+type MutationNamespace<ModuleName, ModuleType> = <Key extends keyof ModuleType>(namespace: ModuleName, map: Key[]) => {
+  [K in Key]: (...p: ParamsType<ModuleType[K]>) => void;
 }
-// @ts-ignore
-export const mapMutationsTyped = _mapMutationsTyped;
+
+interface MutationMapperWithNamespace extends ModulesMutation {
+
+}
+
+const _mapMutationsTyped = mapMutations;
+export const mapMutationsTyped: MutationMapper & MutationMapperWithNamespace =
+  process.env.NODE_ENV === "production"
+    ? _mapMutationsTyped
+    : function(...args: any[]) {
+        // @ts-ignore
+        const res = _mapMutationsTyped(...args) as any;
+        return res;
+      };
+
+// ==================== mapGetters ===================
+// TODO： 后期有时间添加
