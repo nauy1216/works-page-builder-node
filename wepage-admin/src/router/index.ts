677f6f5294ab760db9dad78922e44219aec67495
liuchengyuan <liucy@rp-pet.cn>
2020-10-23 17:44:50 +0800

    update

--- wepage-admin/src/router/index.ts
+++ wepage-admin/src/router/index.ts
@@ -33,7 +33,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/test",
     name: "test",
-    component: () => import(/* webpackChunkName: "about" */ "../views/test/test1.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/test/index.vue")
   }
 ];
 
--- wepage-admin/src/views/test/index.vue
+++ wepage-admin/src/views/test/index.vue
@@ -1,21 +1,42 @@
 <script lang="tsx">
-export default {
+import Vue from "vue";
+export default Vue.extend({
   data() {
     return {
       msg: 9,
       show: true
     };
   },
-  render() {
-    return (
-      <div>
-        <div style="transform: translate(0px, 0px); background: red; width: 1920px; height: 100px; text-align:right">222</div>
-        <div style="transform: translate(0px, 0px); background: green; width: 100%; height: 100px"></div>
-        <div style="transform: translate(0px, 0px); background: yellow; width: 100vw; height: 100px"></div>
-      </div>
+  created() {
+    window.addEventListener(
+      "message",
+      e => {
+        if (e.data.from === "wepage-web") {
+          console.log("接受iframe的消息", e.data);
+        }
+      },
+      false
     );
+  },
+  methods: {
+    sendMessage() {
+      const win = (this.$refs.app as any).contentWindow as Window;
+      win.postMessage(
+        {
+          from: "wepage-admin",
+          type: "setAppConfig",
+          data: {
+            a: 100
+          }
+        },
+        "*"
+      );
+    }
+  },
+  render() {
+    return <iframe ref="app" src="http://localhost:8002/appID" style="width: 100vw; height: 100vh;"></iframe>;
   }
-};
+});
 </script>
 
 <style></style>
