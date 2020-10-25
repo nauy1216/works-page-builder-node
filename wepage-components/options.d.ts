d9b9f5967a7965a8ccdef07bf22d991365056412
liuchengyuan <2267698673@qq.com>
2020-10-25 18:24:49 +0800

    update

--- wepage-components/options.d.ts
+++ wepage-components/options.d.ts
@@ -1,14 +1,22 @@
-declare module "vue/types/options" {
-    interface PropOptions {
-      label?: string; // 属性名称，用于展示
-      inputComponent?: string; // 用于编辑该属性的组件
-    }
-    interface ComponentOptions<V extends Vue> {
-      config?: {
-        alias: string; // 组件名称
-        icon: string; // 组件图标
-        [k: string]: any
-      };
-    }
+
+import Vue from "vue";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $eventBus: Vue;
+    $compList: { [key: string]: CustomVueConstructor };
   }
-  
+}
+declare module "vue/types/options" {
+  interface PropOptions {
+    label?: string; // 属性名称，用于展示
+    inputComponent?: string; // 用于编辑该属性的组件
+  }
+  interface ComponentOptions<V extends Vue> {
+    config?: {
+      alias: string; // 组件名称
+      icon: string; // 组件图标
+      [key: string]: any
+    };
+  }
+}
